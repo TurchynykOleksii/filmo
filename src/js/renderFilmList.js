d18c4378a7ae8e707eb805f93b698ec44c8fd18a
Turchynyk Oleksii <105796938+TurchynykOleksii@users.noreply.github.com>
2022-12-15 02:40:01 +0200

    fix genres name in film card

--- src/js/renderFilmList.js
+++ src/js/renderFilmList.js
@@ -1,5 +1,6 @@
 import ApiFilm from './apiServises';
 import refs from './refs';
+
 const onLoadListFilm = new ApiFilm();
 
 const loadTrendingFilm = async () => {
@@ -14,17 +15,34 @@ const loadGenreFilm = async () => {
   return genres;
 };
 
+const findGenreName = ids => {
+  const genres = JSON.parse(localStorage.getItem('allGenres'));
+
+  let res = [];
+  for (const key of ids) {
+    let resItem = genres.find(genre => genre.id === Number(key));
+
+    resItem.name !== 'Science Fiction'
+      ? res.push(resItem.name)
+      : res.push('Sci-Fi');
+  }
+
+  const formattedGenres =
+    res.length > 2 ? `${res[0]}, ${res[1]}, Other` : res.join(', ');
+  return formattedGenres;
+};
+
 const renderFilm = async () => {
   const markup = await loadTrendingFilm();
-  const genre = await loadGenreFilm();
 
   const allFilms = markup.map(item => {
-    //console.log(item);
     let getYear = parseInt(item.release_date);
+
     return `
      <div class="film" id='${item.id}' data-ids="${item.genre_ids}">
       <img src='https://image.tmdb.org/t/p/w500${item.backdrop_path}'>
       <p class="film-headline">${item.title}</p>
+      <span>${findGenreName(item.genre_ids)}</span>
       <p>${getYear}</p>
      </div>
     `;
